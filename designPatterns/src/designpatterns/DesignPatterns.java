abe4bbb677e4e7146a971103425ce1e27b263846
JJacoboJ125 <111005081+JJacoboJ125@users.noreply.github.com>
2022-11-21 19:30:19 -0500

    Update DesignPatterns.java

--- designPatterns/src/designpatterns/DesignPatterns.java
+++ designPatterns/src/designpatterns/DesignPatterns.java
@@ -14,7 +14,7 @@ public class DesignPatterns {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("no entiendo un culoooooo");
+        System.out.println("");
     }
     
 }
